bc0f81085aced061d8b40139c576ebd2ea3dcecb
MRef02 <marifercan73@gmail.com>
2025-11-08 22:31:27 -0600

    segunda actualizacion de mano
    cambio en el nombre  de los parametros

--- Interfaces/IMano.cs
+++ Interfaces/IMano.cs
@@ -6,6 +6,6 @@ namespace BlackJack_Uno_BackUp.Clases;
 interface IMano
 {  
     public void AgregarCarta(Carta carta);
-    public carta QuitarCarta(Carta carta);
+    public Carta QuitarCarta(Carta carta);
 
 }
