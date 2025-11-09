6f29599a6180bb709e9f5865747849f53161c7e4
MRef02 <marifercan73@gmail.com>
2025-11-08 23:12:05 -0600

    Cambios Semanticos y validaciones
    se hicieron cambios cambios en los accesores debido a errores  y tambien de espacios en el codigo

--- Clases/BlackJack/ManoBJ.cs
+++ Clases/BlackJack/ManoBJ.cs
@@ -5,6 +5,11 @@ namespace BlackJack_Uno_BackUp.Clases.BlackJack;
 class ManoBJ:Mano, IMano
 {
     public override List<Carta> ManoCartas
+    {
+        get { return _ManoCartas; }
+        set { _ManoCartas = value; }
+    }
+    
     public void AgregarCarta(Carta carta)
     {
         ManoCartas.Add(carta);
@@ -14,7 +19,7 @@ class ManoBJ:Mano, IMano
     {
         if (!ManoCartas.Contains(carta))
         {
-             throw new Exception("La carta no se encuentra en la mano");
+            throw new Exception("La carta no se encuentra en la mano");
         }
         
         ManoCartas.Remove(carta);
--- Clases/UNO/ManoUNO.cs
+++ Clases/UNO/ManoUNO.cs
@@ -7,6 +7,11 @@ namespace BlackJack_Uno_BackUp.Clases.UNO;
 class ManoUNO:Mano, IMano
 {
     public override List<Carta> ManoCartas
+    {
+        get { return _ManoCartas; }
+        set { _ManoCartas = value; }
+    }
+    
     public void AgregarCarta(Carta carta)
     {
         ManoCartas.Add(carta);
@@ -16,7 +21,7 @@ class ManoUNO:Mano, IMano
     {
         if (!ManoCartas.Contains(carta))
         {
-             throw new Exception("La carta no se encuentra en la mano");
+         throw new Exception("La carta no se encuentra en la mano");
         }
         
         ManoCartas.Remove(carta);
