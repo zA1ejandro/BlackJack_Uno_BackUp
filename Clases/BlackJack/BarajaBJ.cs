b621ee6eb2b6de4bfd78c7a5df66cb6854806e8b
zA1ejandro <alexmocu241106@gmail.com>
2025-11-08 20:14:22 -0600

    Creacion de Baraja BlackJack
    Se creo la estructura de una baraja de black jack y la implementacion de como creara la baraja

---
+++ Clases/BlackJack/BarajaBJ.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Drawing;
+using BlackJack_Uno_BackUp.Interfaces;
+
+namespace BlackJack_Uno_BackUp.Clases.BlackJack;
+
+class BarajaBJ:Baraja,ICrearBaraja
+{
+    public override List<Carta> BarajaCartas
+    {
+        get { return _barajaCartas; }
+        set
+        {
+            if (value.Count > 52)
+            {
+                throw new Exception("La baraja de BlackJack solo puede tener 52 cartas");
+            }
+            _barajaCartas = value;
+        }
+    }
+
+    private static readonly Carta.Colores[] ColoresPosibles =
+    {
+        Carta.Colores.Rojo,Carta.Colores.Negro
+    };
+
+    private static readonly CartaBJ.Figuras[] Figuras =
+    {
+        CartaBJ.Figuras.Corazon,CartaBJ.Figuras.Trebol,CartaBJ.Figuras.Diamante,CartaBJ.Figuras.Pica
+    };
+
+    public List<Carta> CrearBaraja()
+    {
+        List<Carta> BarajaCreada = new List<Carta>();
+        int alternarColor = 0;
+        foreach (var palo in Figuras)
+        {
+            for (int i = 2; i < 11; i++)
+            {
+                BarajaCreada.Add(new CartaBJ(i, ColoresPosibles[alternarColor], palo));
+            }
+            alternarColor++;
+            if (alternarColor > 1)
+            {
+                alternarColor = 0;
+            }
+        }
+        return BarajaCreada;
+    }
+    
+    public BarajaBJ()
+    {
+        BarajaCartas = CrearBaraja();
+    }
+}
