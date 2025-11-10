073b4cf2be0343f532201b18d4d42977faa303a3
zA1ejandro <alexmocu241106@gmail.com>
2025-11-09 21:54:42 -0600

    Creacion de juego base
    Se creo la estructura base de los juegos, sigue como propuesta esta a disposición de cambios

---
+++ Interfaces/IJuego.cs
@@ -0,0 +1,10 @@
+using System;
+using BlackJack_Uno_BackUp.Clases;
+
+namespace BlackJack_Uno_BackUp.Interfaces;
+
+interface IJuego
+{
+    public void Jugar();
+    public Juego InicializarJuego();
+}
